984ea160e16dc1e67c6be753f33ea27b3db009a6
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2022-10-01 21:42:51 +0600

    dsfds

--- 2 lesson/ViewController.swift
+++ 2 lesson/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
